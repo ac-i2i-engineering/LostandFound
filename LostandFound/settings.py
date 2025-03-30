e903bdbc02fcdfab0a8d19de2c5c71a52e363355
alston26 <achen26@amherst.edu>
2025-03-30 00:16:54 -0400

    image view

--- LostandFound/settings.py
+++ LostandFound/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Where your static files are located
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
