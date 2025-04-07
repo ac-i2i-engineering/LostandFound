880c89a235e9b24714716c70ec4fd69258e2fbb4
Tetsuya787 <tetsuyatanaka007@gmail.com>
2025-04-07 02:08:49 -0400

    added map

--- locations/models.py
+++ locations/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, help_text="Short description of this location")
+    image = models.ImageField(upload_to='location_images/', blank=True, null=True)
     latitude = models.FloatField()  # remove null=True, blank=True
     longitude = models.FloatField()
     
